cbf37d74273d669293e4abc83953af1b4043fdc2
kimyubi <ieieie0419@gmail.co,=m>
2023-03-26 23:06:54 +0900

    style

--- src/main/java/dev/donmit/donmitapi/auth/dto/OauthTokenResponseDto.java
+++ src/main/java/dev/donmit/donmitapi/auth/dto/OauthTokenResponseDto.java
@@ -3,7 +3,7 @@ package dev.donmit.donmitapi.auth.dto;
 import java.util.Objects;
 
 public record OauthTokenResponseDto(String access_token, String scope, String token_type) {
-	
+
 	public OauthTokenResponseDto {
 		Objects.requireNonNull(access_token);
 		Objects.requireNonNull(scope);
